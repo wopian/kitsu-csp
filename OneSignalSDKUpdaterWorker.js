6f1fb9b2c83ffc72fd239afe29a587b866720c18
James Harris <wopian@wopian.me>
2020-08-02 16:41:01 +0100

    build: init repo

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,3 @@
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js")
+
+//# sourceMappingURL=OneSignalSDKUpdaterWorker-794c6cdad7ce97ee9eb55749f39be718.map
---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,3 @@
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js")
+
+//# sourceMappingURL=OneSignalSDKWorker-f82482152657748f8f89ea6ce453b401.map
